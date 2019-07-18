2c23c87e4ebc3eec2c2bae49a66ed4b47d2c26ae
SowmyaDondeti <sowmyar987@gmail.com>
2019-07-17 19:31:50 -0500

    your awesomely descriptive, yet pithy commit message goes here

---
+++ src/main/java/org/launchcode/projectcycle/springproject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.projectcycle.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
