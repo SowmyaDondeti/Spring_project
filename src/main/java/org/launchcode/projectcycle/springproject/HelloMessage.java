ea9fb87d0a0ded8f13cac40b1a3aa3ccbc7ec8d9
SowmyaDondeti <sowmyar987@gmail.com>
2019-07-30 08:48:03 -0500

    created html file for controller

--- src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
+++ src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
@@ -2,18 +2,16 @@ package org.launchcode.projectcycle.springproject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+
 public class HelloMessage
 {
     @RequestMapping(value = "menu")
-    @ResponseBody
     public String helloUrlSegment(HttpServletRequest request){
-
-        return "My Favorite items in the menu are paneer butter masala and chicken kurma ";
+        return "menu_items.html";
     }
 
 }
