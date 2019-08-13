fc41371314ff2fd467068cdc36fb6886b9f6d40d
SowmyaDondeti <sowmyar987@gmail.com>
2019-08-13 13:14:53 -0500

    Changes made

--- src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
+++ src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
@@ -2,6 +2,7 @@ package org.launchcode.projectcycle.springproject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
