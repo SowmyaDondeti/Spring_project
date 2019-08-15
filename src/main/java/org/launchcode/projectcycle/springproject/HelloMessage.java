61531cb3e25a8a4f1049c41732fea182bcdde2bc
SowmyaDondeti <sowmyar987@gmail.com>
2019-08-15 05:57:42 -0500

    added model,controller and entity

--- src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
+++ src/main/java/org/launchcode/projectcycle/springproject/HelloMessage.java
@@ -2,7 +2,6 @@ package org.launchcode.projectcycle.springproject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
